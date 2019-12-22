cd80bbf170dd743e1c414aa88a5b770aff20fa9d
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 18:07:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+
+def prime?(number)
+ 
+  return false if !number.integer?
+  
+  return false if number < 2
+
+  return true if number == 2
+  
+  (2..number-1).each {|int|
+   return false if number % int == 0 }
+  
+  true
+
+end
