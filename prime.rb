a696f3774ffe263b77126e2ff47333c25a2f1784
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 18:03:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 
 
 def prime(number)
- 
+ na << (2..number-1).to_a
   return false if !number.integer?
   
   return false if number < 2
 
   return true if number == 2
   
-  [2..number-1].each {|int|
+  na.each {|int|
    return false if number % int == 0 }
   
   true
