e5b9d671ecb2e8a5cacb243b5143b24d35c6d054
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 17:56:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@
 def prime(number)
   return false if !number.integer?
   
-  
+  return false if number < 2
 
-  return true if number == (1,2,3) 
+  return true if number == 2
   
   (2..number-1).each {|int|
    return false if number % int == 0 }
