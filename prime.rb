0b8c83d7da27374827e66cbe0b8dd744b3743f4a
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 17:42:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+
+def prime(number)
+ na = [] << (2..number-1).to_a
+ 
+ 
+  na.any? { |n|
+    if number % n == 0 
+      false
+    else
+      true
+    end                }
+ 
+
+end
